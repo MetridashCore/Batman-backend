73b00eead7b311b5a2a7e92dc7b76d26af21cdfd
Mr. Robot <gomexxxx71@gmail.com>
2023-08-03 16:23:46 +0600

    create customer

--- routers/customers.ts
+++ routers/customers.ts
@@ -8,4 +8,18 @@ router.get("/", async (req, res) => {
   res.send(customers);
 });
 
+router.post("/", async (req, res) => {
+  const { name, email, contact } = req.body;
+  try {
+    await razorpay.customers.create({
+      name,
+      contact,
+      email,
+    });
+    return res.send("customer created");
+  } catch (error) {
+    return res.send("Error");
+  }
+});
+
 export default router;
