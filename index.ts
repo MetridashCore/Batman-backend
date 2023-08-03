3bc2b524aba31ce0dcef2b60d085495822f655b6
Mr. Robot <gomexxxx71@gmail.com>
2023-08-03 15:40:36 +0600

    build customers router

--- index.ts
+++ index.ts
@@ -1,5 +1,12 @@
 import express from "express";
+import customerRouter from "./routers/customers";
 
 const app = express();
+
+app.use("/api/customers", customerRouter);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.listen(8000, () => console.log("Server started"));
