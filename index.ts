6dc51713aebe132144f366b6014f51a797f7e28b
Mr. Robot <gomexxxx71@gmail.com>
2023-08-04 15:13:12 +0600

    add helmet

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import express from "express";
+import helmet from "helmet";
 import customerRouter from "./routers/razorpay/customers";
 import OrderRouter from "./routers/razorpay/order";
 import accountRouter from "./routers/razorpay/account";
@@ -7,6 +8,7 @@ import accountRouter from "./routers/razorpay/account";
 const app = express();
 
 // Middleware
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/razorpay/api/customers", customerRouter);
