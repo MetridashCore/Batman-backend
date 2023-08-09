cc86a6f2fe56a9494f25ecb0993a7dc1db019954
Mr. Robot <gomexxxx71@gmail.com>
2023-08-09 20:26:36 +0600

    build order route for banking and upi

--- routers/razorpay/order.ts
+++ routers/razorpay/order.ts
@@ -11,6 +11,9 @@ const schema = z.object({
       required_error: "The amount must be at least INR 1.00",
     })
     .min(1, { message: "Amount must be less than or equal to 1.00 INR" }),
+  account_number: z.string().min(1, { message: "Account Number is required" }),
+  name: z.string().min(1, { message: "Name is required" }),
+  ifsc: z.string().min(1, { message: "IFSC is required" }),
 });
 
 router.get("/", async (req, res) => {
@@ -29,6 +32,28 @@ router.post("/", async (req, res) => {
       amount: amount * 100,
       currency: "INR",
       partial_payment: false,
+    });
+    return res.send(order);
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return res.send(error.issues[0].message);
+    }
+    return res.send(error);
+  }
+});
+
+router.post("/banking/upi", async (req, res) => {
+  try {
+    const { amount, account_number, name, ifsc } = schema.parse(req.body);
+    const order = await razorpay.orders.create({
+      amount: amount * 100,
+      currency: "INR",
+      partial_payment: false,
+      bank_account: {
+        account_number,
+        name,
+        ifsc,
+      },
     });
     return res.send(order);
   } catch (error) {
