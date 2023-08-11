e01acae71e7d3f1b7c880c730334e15328483305
Mr. Robot <gomexxxx71@gmail.com>
2023-08-11 17:20:47 +0600

    build validation for fund

--- routers/razorpay/fund.ts
+++ routers/razorpay/fund.ts
@@ -1,16 +1,27 @@
 import { Router } from "express";
+import { z } from "zod";
 import razorpay from "../../services/razorpay";
 
 const router = Router();
 
+const schema = z.object({
+  name: z.string().min(1, { message: "Name is required" }),
+  account_number: z.string().min(1, { message: "Account Number is required" }),
+  ifsc: z.string().min(1, { message: "IFSC is required" }),
+});
+
 router.get("/:customerId", async (req, res) => {
-  const allFund = await razorpay.fundAccount.fetch(req.params.customerId);
-  return res.send(allFund);
+  try {
+    const allFund = await razorpay.fundAccount.fetch(req.params.customerId);
+    return res.send(allFund);
+  } catch (error) {
+    return res.send(error);
+  }
 });
 
 router.post("/:customerId", async (req, res) => {
-  const { name, account_number, ifsc } = req.body;
   try {
+    const { name, account_number, ifsc } = schema.parse(req.body);
     const fund = await razorpay.fundAccount.create({
       customer_id: req.params.customerId,
       account_type: "bank_account",
@@ -21,8 +32,11 @@ router.post("/:customerId", async (req, res) => {
       },
     });
     return res.send(fund);
-  } catch (ex) {
-    return res.send("Error");
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return res.send(error.issues[0].message);
+    }
+    return res.send(error);
   }
 });
 
