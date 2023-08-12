bf5349cf5b50d97ebb2eefcb104d6df4123a8c55
Mr. Robot <gomexxxx71@gmail.com>
2023-08-12 23:40:50 +0600

    run test for customers

---
+++ tests/customers.spec.ts
@@ -0,0 +1,25 @@
+import razorpay from "../services/razorpay";
+
+describe("/api/razorpay/customers", () => {
+  describe("GET /", () => {
+    it("should return all customers", async () => {
+      const customers = await razorpay.customers.all();
+      if (customers.items.length > 0) {
+        for (const customer of customers.items) {
+          expect(customer).toHaveProperty("id");
+          expect(customer).toHaveProperty("name");
+          expect(customer).toHaveProperty("email");
+          expect(customer).toHaveProperty("contact");
+        }
+      }
+    });
+    it("should return single customer", async () => {
+      const customer = await razorpay.customers.fetch("cust_MNZ5p8CBJ5DSbD");
+      expect(customer).toBeTruthy();
+      expect(customer).toHaveProperty("id");
+      expect(customer).toHaveProperty("name");
+      expect(customer).toHaveProperty("email");
+      expect(customer).toHaveProperty("contact");
+    });
+  });
+});
