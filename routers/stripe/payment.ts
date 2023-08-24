e3d827337d487acc7ae5f210b6c683cb6ac8a674
Mr. Robot <gomexxxx71@gmail.com>
2023-08-24 20:09:05 +0600

    refactor

--- routers/stripe/payment.ts
+++ routers/stripe/payment.ts
@@ -1,39 +1,33 @@
 import { Router } from "express";
+import stripe from "../../services/stripe";
+
 const router = Router();
-const cors = require("cors");
-const stripe = require("stripe")(process.env.STRIPE_SECRET);
-import express from "express";
-router.use(express.json());
-router.use(cors());
 
 // checkout api
-router.post("/",async(req,res)=>{
-    const {products} = req.body;
-    console.log(products)
+router.post("/", async (req, res) => {
+  const { products } = req.body;
 
-    const session = await stripe.checkout.sessions.create({
-        payment_method_types:["card"],
-        line_items: [
-            {
-              price_data: {
-                currency: "usd",
-                unit_amount: products.prices * 100,
-                product_data: {
-                  name: `${products.tokens} tokens/${products.words} words`,
-                },
-                
-              },
-             
-              quantity: 1, // Add this line to specify the quantity
-            },
-          ],
-        mode:"payment",
-        success_url:"http://localhost:3000/pricing",
-        cancel_url:"http://localhost:3000/pricing",
-    });
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price_data: {
+          currency: "usd",
+          unit_amount: products.prices * 100,
+          product_data: {
+            name: `${products.tokens} tokens/${products.words} words`,
+          },
+        },
 
-    res.json({id:session.id})
- 
-})
+        quantity: 1, // Add this line to specify the quantity
+      },
+    ],
+    mode: "payment",
+    success_url: "http://localhost:3000/pricing",
+    cancel_url: "http://localhost:3000/pricing",
+  });
+
+  res.json({ id: session.id });
+});
 
 export default router;
--- startup/prod.ts
+++ startup/prod.ts
@@ -1,11 +1,13 @@
 import type { Express } from "express";
 import helmet from "helmet";
 import compression from "compression";
+import cors from "cors";
 import * as Sentry from "@sentry/node";
 
 export default function (app: Express) {
   app.use(Sentry.Handlers.requestHandler());
   app.use(Sentry.Handlers.tracingHandler());
+  app.use(cors());
   app.use(helmet());
   app.use(compression());
 }
