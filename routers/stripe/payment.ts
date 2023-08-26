27f5de4cdab47775049f6315082ac6584b988b2c
chinweokwu <paulcmorah@gmail.com>
2023-08-26 16:02:47 +0100

    update payment routes

--- routers/stripe/payment.ts
+++ routers/stripe/payment.ts
@@ -1,6 +1,5 @@
 import { Router, Request, Response } from "express";
 import stripe from "../../services/stripe";
-import { generateApiKey } from "generate-api-key";
 import admin from "./../../firebase";
 
 declare global {
@@ -15,95 +14,47 @@ const DOMAIN = 'http://localhost:1337'
 
 const router = Router();
 
-
-
-router.get('/api', async (req: Request, res: Response) => {
-  const { api_key } = req.query;
-  if (!api_key) { return res.sendStatus(403); }
-  let paid_status: boolean = false;
-  const doc = await admin.firestore().collection('api_keys').doc(api_key as string).get();
-  if (!doc.exists) {
-    res.status(403).send({ 'status': "API Key is invalid" });
-  } else {
-    const { status, type: keyType, stripeCustomerId } = doc.data()!;
-    if (status === 'subscription') {
-      paid_status = true;
-      const customer = await stripe.customers.retrieve(
-        stripeCustomerId,
-        { expand: ['subscriptions'] }
-      );
-      console.log(customer);
-
-      let subscriptionId = customer?.subscriptions?.data?.[0]?.id;
-      console.log(subscriptionId);
-      const subscription = await stripe.subscriptions.retrieve(subscriptionId!);
-      const itemId = subscription?.items?.data[0].id;
-
-      const record = stripe.subscriptionItems.createUsageRecord(
-        itemId, {
-        quantity: 1,
-        timestamp: 'now',
-        action: 'increment'
-      }
-      );
-      console.log('record created');
-    } else if (status > 0) {
-      paid_status = true;
-      const data = {
-        status: status - 1
-      };
-      const response = await admin.firestore().collection('api_keys').doc(api_key as string).set(data, { merge: true });
-    }
-  }
-  if (paid_status) {
-    res.status(200).send({ "message": "You can do it! I believe in you! Don't give up yet!" });
-  } else {
-    res.sendStatus(403);
-  }
-});
-
-router.get('/check_status', async (req: Request, res: Response) => {
-  const { api_key } = req.query;
-  const doc = await admin.firestore().collection('api_keys').doc(api_key as string).get();
-  if (!doc.exists) {
-    res.status(400).send({ 'status': "API Key does not exist" });
-  } else {
-    const { status } = doc.data() ?? {};
-    res.status(200).send({ 'status': status });
-  }
-});
-
 router.get('/delete', async (req: Request, res: Response) => {
-  const { api_key } = req.query;
-  const doc = await admin.firestore().collection('api_keys').doc(api_key as string).get();
+  const userId = req.user?.uid;
+  const doc = await admin.firestore().collection('api_keys').doc().get();
+  
   if (!doc.exists) {
     res.status(400).send({ 'status': "API Key does not exist" });
   } else {
-    const { stripeCustomerId } = doc.data() ?? {};
-    try {
-      const customer = await stripe.customers.retrieve(
-        stripeCustomerId,
-        { expand: ['subscriptions'] }
-      );
-      console.log(customer);
-      let subscriptionId = customer?.subscriptions?.data?.[0]?.id;
-      stripe.subscriptions.del(subscriptionId);
+    const { stripeCustomerId, userIds } = doc.data() ?? {};
 
-      const data = {
-        status: null
-      };
-      const response = await admin.firestore().collection('api_keys').doc(api_key as string).set(data, { merge: true });
-    } catch (err) {
-      return res.sendStatus(500);
+    if (!userIds.includes(userId)) {
+      res.status(403).send({ 'status': "Access denied" });
+    } else {
+      try {
+        const customer = await stripe.customers.retrieve(
+          stripeCustomerId,
+          { expand: ['subscriptions'] }
+        );
+        console.log(customer);
+        let subscriptionId = customer?.subscriptions?.data?.[0]?.id;
+        
+        if (subscriptionId && customer.subscriptions.data[0].status !== 'incomplete') {
+          await stripe.subscriptions.del(subscriptionId);
+        }
+
+        const data = {
+          status: null
+        };
+        await admin.firestore().collection('api_keys').doc().set(data, { merge: true });
+        
+        res.status(200).send({ 'status': 'Subscription cancelled' });
+      } catch (err) {
+        return res.sendStatus(500);
+      }
     }
-    res.sendStatus(200);
   }
 });
 
 router.post('/create-checkout-session/:product', async (req: Request, res: Response) => {
   const { product } = req.params;
   const userId = req.user?.uid;
-  let mode: string, price_ID: string, line_items: any[], quantity_type: any;
+  let mode: string, price_ID: string, line_items: any[], quantity_type: any, token: string;
 
   if (product === 'pre1') {
     price_ID = 'price_1NiyeaGo36tEddLKI8NHcixv';
@@ -159,45 +110,30 @@ router.post('/create-checkout-session/:product', async (req: Request, res: Respo
     return res.sendStatus(403);
   }
 
-  const newAPIKey = generateApiKey();
-  const customer = await stripe.customers.create({
-    metadata: {
-      APIkey: newAPIKey,
-      userId: userId 
-    }
-  });
+  const customer = await stripe.customers.create();
 
   const stripeCustomerId = customer.id;
   const session = await stripe.checkout.sessions.create({
     customer: stripeCustomerId,
-    metadata: {
-      APIkey: newAPIKey,
-      payment_type: product
-    },
     line_items: line_items,
     mode: mode,
-    success_url: `${DOMAIN}/success.html?api_key=${newAPIKey}`,
+    success_url: `${DOMAIN}/success.html`,
     cancel_url: `${DOMAIN}/cancel.html`,
   });
   console.log(session);
 
+  token = quantity_type.toString();
 
   const data = {
-    APIkey: newAPIKey,
     payment_type: product,
     stripeCustomerId,
     status: quantity_type,
-    userId: userId 
+    userId: userId,
+    token: token 
   };
-  const response = await admin.firestore().collection('api_keys').doc(newAPIKey as string).set(data, { merge: true });
+  const response = await admin.firestore().collection('api_keys').doc().set(data, { merge: true });
 
   res.redirect(303, session.url);
 });
-
-router.post('/stripe_webhook', (req: Request, res: Response) => {
-
-});
-
-
 
 export default router;
