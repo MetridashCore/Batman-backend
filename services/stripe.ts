a54cae99ec0d7e5427ec5df3235f37b7f0569333
iiio2 <gomexxxx71@gmail.com>
2024-07-08 13:05:17 +0600

    fix: stripe configuration

--- services/stripe.ts
+++ services/stripe.ts
@@ -1,5 +1,7 @@
-import stripe from "stripe"
+import Stripe from "stripe"
 
-const stripePackage = stripe(process.env.STRIPE_SECRET)
+const stripePackage = new Stripe(process.env.STRIPE_SECRET!, {
+    apiVersion:'2023-08-16'
+})
 
 export default stripePackage
