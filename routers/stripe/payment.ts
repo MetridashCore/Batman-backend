f0e786de5081d0c63626e5263d8355f722bde648
Mr. Robot <gomexxxx71@gmail.com>
2024-07-28 18:53:08 +0600

    chore: add base url from env

--- routers/stripe/payment.ts
+++ routers/stripe/payment.ts
@@ -1,18 +1,18 @@
-import { Router } from "express";
-import stripe from "../../services/stripe";
+import { Router } from 'express'
+import stripe from '../../services/stripe'
 
-const router = Router();
+const router = Router()
 
 // checkout api
-router.post("/", async (req, res) => {
-  const { products } = req.body;
+router.post('/', async (req, res) => {
+  const { products } = req.body
 
   const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
+    payment_method_types: ['card'],
     line_items: [
       {
         price_data: {
-          currency: "usd",
+          currency: 'usd',
           unit_amount: products.prices * 100,
           product_data: {
             name: `${products.tokens} tokens/${products.words} words`,
@@ -22,12 +22,11 @@ router.post("/", async (req, res) => {
         quantity: 1, // Add this line to specify the quantity
       },
     ],
-    mode: "payment",
-    success_url: "http://localhost:3000/pricing",
-    cancel_url: "http://localhost:3000/pricing",
-  });
+    mode: 'payment',
+    success_url: `${process.env.BASE_URL!}/success`,
+    cancel_url: `${process.env.BASE_URL}/cancel`,
+  })
+  res.json({ id: session.id })
+})
 
-  res.json({ id: session.id });
-});
-
-export default router;
+export default router
