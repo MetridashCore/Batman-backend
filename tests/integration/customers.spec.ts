aea8f9ab664beb46d0ac3d202f67d0ba3d190e31
Mr. Robot <gomexxxx71@gmail.com>
2024-07-29 20:30:57 +0600

    refactor: move folders and entry point to `src`

--- tests/integration/customers.spec.ts
+++ tests/integration/customers.spec.ts
@@ -1,40 +1,40 @@
-import razorpay from "../../services/razorpay";
+import razorpay from '../../src/services/razorpay'
 
-describe("/api/razorpay/customers", () => {
-  describe("GET /", () => {
-    it("should return all customers", async () => {
-      const customers = await razorpay.customers.all();
+describe('/api/razorpay/customers', () => {
+  describe('GET /', () => {
+    it('should return all customers', async () => {
+      const customers = await razorpay.customers.all()
       if (customers.items.length > 0) {
         for (const customer of customers.items) {
-          expect(customer).toHaveProperty("id");
-          expect(customer).toHaveProperty("name");
-          expect(customer).toHaveProperty("email");
-          expect(customer).toHaveProperty("contact");
+          expect(customer).toHaveProperty('id')
+          expect(customer).toHaveProperty('name')
+          expect(customer).toHaveProperty('email')
+          expect(customer).toHaveProperty('contact')
         }
       }
-    });
-    it("should return single customer", async () => {
-      const customer = await razorpay.customers.fetch("cust_MNZ5p8CBJ5DSbD");
-      expect(customer).toBeTruthy();
-      expect(customer).toHaveProperty("id");
-      expect(customer).toHaveProperty("name");
-      expect(customer).toHaveProperty("email");
-      expect(customer).toHaveProperty("contact");
-    });
-  });
-  describe("POST /", () => {
-    it("should create single customer", async () => {
+    })
+    it('should return single customer', async () => {
+      const customer = await razorpay.customers.fetch('cust_MNZ5p8CBJ5DSbD')
+      expect(customer).toBeTruthy()
+      expect(customer).toHaveProperty('id')
+      expect(customer).toHaveProperty('name')
+      expect(customer).toHaveProperty('email')
+      expect(customer).toHaveProperty('contact')
+    })
+  })
+  describe('POST /', () => {
+    it('should create single customer', async () => {
       try {
         const newCustomerData = {
-          name: "New Customer",
-          email: "new@example.com",
+          name: 'New Customer',
+          email: 'new@example.com',
           contact: +9184938274,
-        };
-        const customer = await razorpay.customers.create(newCustomerData);
-        expect(customer).toBeTruthy();
+        }
+        const customer = await razorpay.customers.create(newCustomerData)
+        expect(customer).toBeTruthy()
       } catch (error: any) {
-        expect(error.statusCode).toBe(400);
+        expect(error.statusCode).toBe(400)
       }
-    });
-  });
-});
+    })
+  })
+})
--- tests/integration/order.spec.ts
+++ tests/integration/order.spec.ts
@@ -1,25 +1,25 @@
-import razorpay from "../../services/razorpay";
+import razorpay from '../../src/services/razorpay'
 
-describe("/api/razorpay/order", () => {
-  describe("GET /", () => {
-    it("should return all orders", async () => {
-      const orders = await razorpay.orders.all();
+describe('/api/razorpay/order', () => {
+  describe('GET /', () => {
+    it('should return all orders', async () => {
+      const orders = await razorpay.orders.all()
       if (orders.items.length > 0) {
         for (const order of orders.items) {
-          expect(order).toHaveProperty("id");
-          expect(order).toHaveProperty("entity");
-          expect(order).toHaveProperty("amount");
-          expect(order).toHaveProperty("currency");
+          expect(order).toHaveProperty('id')
+          expect(order).toHaveProperty('entity')
+          expect(order).toHaveProperty('amount')
+          expect(order).toHaveProperty('currency')
         }
       }
-    });
-    it("should return single order", async () => {
-      const order = await razorpay.orders.fetch("order_MO02vp2aJAnNKe");
-      expect(order).toBeTruthy();
-      expect(order).toHaveProperty("id");
-      expect(order).toHaveProperty("amount");
-      expect(order).toHaveProperty("currency");
-      expect(order).toHaveProperty("entity");
-    });
-  });
-});
+    })
+    it('should return single order', async () => {
+      const order = await razorpay.orders.fetch('order_MO02vp2aJAnNKe')
+      expect(order).toBeTruthy()
+      expect(order).toHaveProperty('id')
+      expect(order).toHaveProperty('amount')
+      expect(order).toHaveProperty('currency')
+      expect(order).toHaveProperty('entity')
+    })
+  })
+})
